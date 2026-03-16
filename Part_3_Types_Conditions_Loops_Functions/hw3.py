6a95227da91b2f2ca3a803fb2f48ed09218ba73d
Matvey Skirda <skirdamatvei@yandex.ru>
2026-03-16 14:33:40 +0300

    feat: add handler for income changes

--- Part_3_Types_Conditions_Loops_Functions/hw3.py
+++ Part_3_Types_Conditions_Loops_Functions/hw3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-UNKNOWN_COMMAND_MSG = "Неизвестная команда!"
-NONPOSITIVE_VALUE_MSG = "Значение должно быть больше нуля!"
-INCORRECT_DATE_MSG = "Неправильная дата!"
-OP_SUCCESS_MSG = "Добавлено"
+UNKNOWN_COMMAND_MSG = "Unknown command!"
+NONPOSITIVE_VALUE_MSG = "Value must be grater than zero!"
+INCORRECT_DATE_MSG = "Invalid date!"
+OP_SUCCESS_MSG = "Added"
 
 
 def is_leap_year(year: int) -> bool:
@@ -28,8 +28,7 @@ def extract_date(maybe_dt: str) -> tuple[int, int, int] | None:
 
 
 def income_handler(amount: float, income_date: str) -> str:
-    ...
-
+    return f"{OP_SUCCESS_MSG} {amount=} {income_date=}"
 
 def main() -> None:
     """Ваш код здесь"""
